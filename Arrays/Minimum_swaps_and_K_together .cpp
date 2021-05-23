48279ca70c8483c3ba78641b7b696ae26e322e66
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-05-24 00:11:42 +0530

    Update Minimum_swaps_and_K_together .cpp

--- Arrays/Minimum_swaps_and_K_together .cpp
+++ Arrays/Minimum_swaps_and_K_together .cpp
@@ -3,24 +3,27 @@
 // Window Sliding Technique
 // elements smaller than k --> Ghar ke Log
 // ghar ki size(window ki size) = Number of ghar ke log
-int ghar_ke_log=0,ans=0,tracker=0;
-    for(int i=0; i<n; i++) // this loop is calculating number of ghar ke log
-    {
-        if(arr[i] <= k)
+int minSwap(int *arr, int n, int k) \
+{
+    int ghar_ke_log=0,ans=0,tracker=0;
+        for(int i=0; i<n; i++) // this loop is calculating number of ghar ke log
         {
-            if(arr[ghar_ke_log] > k) ans++; // this additional line is used to prepare very first window
-            ghar_ke_log++;
-        }                                   // ans--> bahar ke log
-        
+            if(arr[i] <= k)
+            {
+                if(arr[ghar_ke_log] > k) ans++; // this additional line is used to prepare very first window
+                ghar_ke_log++;
+            }                                   // ans--> bahar ke log
+
+        }
+        if(ghar_ke_log == 1) return 0;
+        tracker = ans;
+        for(int j=0; j+ghar_ke_log<n; j++)
+        { // sliding window technique
+
+            if(arr[j] > k) tracker--; // checking if initial window element was ghar ke log or bahar ke log?
+            if(arr[j+ghar_ke_log] > k) tracker++; //checking if element to be added to window is ghar ke log or bahar ke log
+             ans=min(ans, tracker);
+        }
+        return ans;
     }
-    if(ghar_ke_log == 1) return 0;
-    tracker = ans;
-    for(int j=0; j+ghar_ke_log<n; j++)
-    { // sliding window technique
-       
-        if(arr[j] > k) tracker--; // checking if initial window element was ghar ke log or bahar ke log?
-        if(arr[j+ghar_ke_log] > k) tracker++; //checking if element to be added to window is ghar ke log or bahar ke log
-         ans=min(ans, tracker);
-    }
-    return ans;
 }
