462bf0bd865305205fd1c60578d85ed36d4d58cd
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-05-26 23:54:47 +0530

    Day 03

---
+++ Arrays/smallest_subarray_having_sum_greater_than_a_given_value.cpp
@@ -0,0 +1,18 @@
+// problem link: https://practice.geeksforgeeks.org/problems/smallest-subarray-with-sum-greater-than-x5651/1
+// My YouTube Code Explanation video😎: https://www.youtube.com/watch?v=WI8886xXRlU
+
+int sb(int arr[], int n, int x)
+    {
+        int sum=0,start=0,len=n;
+        for(int i=0; i<n; i++)
+        {
+            sum=sum+arr[i];
+            while(sum > x)
+            {
+                len=min(len, i-start+1);
+                sum=sum-arr[start]; // sliding window technique
+                start++;
+            }
+        }
+        return len;
+    }
