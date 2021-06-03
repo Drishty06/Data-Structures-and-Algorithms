272fead818d59b426792e6ba0b7b6ab4c23f46cb
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-03 20:59:32 +0530

    DAY 01

--- Sorting/Sorting_Algorithms/Selectionsort.cpp
+++ Sorting/Sorting_Algorithms/Selectionsort.cpp
@@ -1,1 +1,28 @@
-
+// selection sort algorithm visualizer: https://www.hackerearth.com/practice/algorithms/sorting/selection-sort/visualize/
+// time complexity: BEST CASE:    O(N^2) 
+//                  AVERAGE CASE: O(N^2)
+//                  WORST CASE:   O(N^2)
+// space complexity: O(1)
+// UNSTABLE
+// IN-PLACE ALGORITHM
+// it works on finding minimum element in each iteration and swap it 
+void selectionsort(int arr[], int n) 
+{
+  int min;        
+  // reduces the effective size of the array by one in each iteration
+  for(int i=0; i<n-1 ;i++)  
+  {
+    // assuming the first element to be the minimum of the unsorted array 
+    min = i ;
+    // gives the effective size of the unsorted array 
+    for(int j=i+1; j<n; j++) 
+    {
+      if(arr[j] < arr[min])  
+      {     
+        min = j ;
+      }
+    }
+     // putting minimum element on its proper position.
+    swap(A[min], arr[i]); 
+  }
+}
