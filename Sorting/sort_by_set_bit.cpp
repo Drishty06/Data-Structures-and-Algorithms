b2c08a0c8b667632fc36833f684ac86dc1f2c5ba
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-10 00:51:15 +0530

    DAY 05

--- Sorting/sort_by_set_bit.cpp
+++ Sorting/sort_by_set_bit.cpp
@@ -1,1 +1,15 @@
-// problem link: https://practice.geeksforgeeks.org/problems/sort-by-set-bit-count1153/1
+    // problem link: https://practice.geeksforgeeks.org/problems/sort-by-set-bit-count1153/1#
+    // expected time complexity: O(n log n) and space complexity o(1)
+    // naive approach: using an auxillary array and storing countbits value using bit manipulation = TLE
+    // better approach: since we have a parameter(countbits) on the basis of which array has to be sorted therefore comparators in inbuilt sort is bestly suited
+    //                  and using countbits by bit manipulation = TLE
+    // efficient approach: using STL function __inbuilt_popcount which gives countbits and all this in a comparator function 
+
+    static bool comparator(int a,int b)
+    {
+        return __builtin_popcount(a) > __builtin_popcount(b);
+    }
+    void sortBySetBitCount(int arr[], int n)
+    {
+        stable_sort(arr,arr+n,comparator);
+    }
