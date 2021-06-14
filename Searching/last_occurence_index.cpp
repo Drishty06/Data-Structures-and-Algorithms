759c8d3a0922eb687d7bcabd2ffe0972e817fe91
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-15 00:31:00 +0530

    DAY 10

--- Searching/last_occurence_index.cpp
+++ Searching/last_occurence_index.cpp
@@ -5,21 +5,21 @@
 
 // iterative:
 int last_occ(int arr[], int n, int x)
-{
-  int low = 0, high = n-1;
-  while(low <= high)
-  {
-    int mid = (low+high)/2;
-    if(arr[mid] < x) low = mid+1;
-    else if(arr[mid] > x) high = mid-1;
-    else
     {
-      if(mid != n-1 || arr[mid] != arr[mid+1]) return mid;
-      else low = mid+1;
+      int low = 0, high = n-1;
+      while(low <= high)
+      {
+        int mid = (low+high)/2;
+        if(arr[mid] < x) low = mid+1;
+        else if(arr[mid] > x) high = mid-1;
+        else
+        {
+          if((mid == n-1 || x < arr[mid+1]) && arr[mid] == x) return mid;
+          else low = mid+1;
+        }
+      }
+      return -1;
     }
-  }
-  return -1;
-}
 
 // Recursive:
 int last_occ(int arr[], int low, int high, int n, int x)
