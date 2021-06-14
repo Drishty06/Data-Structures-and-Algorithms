2dc0cc5e25960da0421ebd029a17de871c9ecddc
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-14 15:58:39 +0530

    DAY 10

--- Codechef/summer_heat.cpp
+++ Codechef/summer_heat.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	for(int i=0; i<t ; i++)
+	{
+		int xa,xb,ya,yb;
+		cin>>xa>>xb>>ya>>yb;
+		cout<<(ya/xa)+(yb/xb)<<endl;
+	}
+	return 0;
+}
