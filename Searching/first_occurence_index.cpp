7b504c1eceb59a9cf976c174e9e3b763bffe4704
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-15 00:31:33 +0530

    DAY 10

--- Searching/first_occurence_index.cpp
+++ Searching/first_occurence_index.cpp
@@ -5,21 +5,21 @@
 
 // iterative:
 int first_occ(int arr[], int n, int x)
-{
-  int low=0, high=n-1;
-  while(low >= high)
-  {
-    int mid = (low+high)/2;
-    if(arr[mid] > x) high = mid-1;
-    else if(arr[mid] < x) low = mid+1;
-    else
     {
-      if(mid == 0 || arr[mid-1] != arr[mid]) return mid;
-      else high = mid-1;
+      int low=0, high=n-1;
+      while(low <= high)
+      {
+        int mid = (low+high)/2;
+        if(arr[mid] > x) high = mid-1;
+        else if(arr[mid] < x) low = mid+1;
+        else
+        {
+          if((mid == 0 || x > arr[mid-1]) && arr[mid] == x) return mid;
+          else high = mid-1;
+        }
+      }
+      return -1;
     }
-  }
-  return -1;
-}
 
 // recursive
 int first_occ(int arr[], int low, int high, int x, int n)
