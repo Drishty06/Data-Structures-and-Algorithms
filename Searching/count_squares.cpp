0c815931b039008299f9ba2ad14e0bc5c249b2ed
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-15 21:58:32 +0530

    DAY 01

--- Searching/count_squares.cpp
+++ Searching/count_squares.cpp
@@ -1,1 +1,13 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+int countSquares(int a, int b)
+{
+    return (floor(sqrt(b)) - ceil(sqrt(a)) + 1);
+}
+int main()
+{
+    int a = 9, b = 25;
+    cout << "Count of squares is "<< countSquares(a, b);
+    return 0;
+}
