62a9b1e08a9be4a5013bb13ff9122649c6a2eb8e
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-24 23:39:58 +0530

    DAY 15

--- Hashing/count_frequency.cpp
+++ Hashing/count_frequency.cpp
@@ -1,1 +1,12 @@
-
+void countfreq(int arr[], int n)
+{
+  unordered_map <int, int> mp;
+  for(int i=0; i<n; i++)
+  {
+    mp[arr[i]]++; // if arr[i] is seen for the first time then it creates new key and value else it simply increments
+  }
+  for(auto x: mp)
+  {
+    cout<<x.first<<" "<<x.second<<endl;
+  }
+}
