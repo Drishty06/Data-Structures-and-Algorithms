2734c5d1f40e2fe3da52b4d4021d65e133816355
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-26 23:30:44 +0530

    June lunchtime 2021

---
+++ Codechef/html_tags.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{ 
+	long int t;
+	cin>>t;
+	for(int i=0; i<t ; i++)
+	{
+		string str;
+		cin>>str;
+		long int n=str.length();
+		if(n <= 3) cout<<"Error"<<endl; // to check for the edge case when the string length is smaller than 3 and empty body isn't allowed as well
+    
+		if(str[0] == '<' && str[1] == '/' && str[n-1] == '>') // to check for the basic syntax </>
+		{
+		    for(int j=2; j<n-1; j++)
+		    {
+		        if((str[j] >= 'a' && str[j] <= 'z' ) || str[j] >= '0' && str[j] <= '9') // to check if body is aplha numeric
+		        {
+		            if(j == n-2) cout<<"Success"<<endl;
+		        }
+		        else 
+		        {
+		            cout<<"Error"<<endl;
+		            break;
+		        }
+		        
+		    }
+		    
+		}
+		else 
+		{
+		    cout<<"Error"<<endl;
+		}
+	}
+	return 0;
+}
