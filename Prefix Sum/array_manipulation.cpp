b1955a727bbe3c9de70f505fb04376b8fe89cdc7
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-06-28 00:02:53 +0530

    DAY 18

--- Prefix Sum/array_manipulation.cpp
+++ Prefix Sum/array_manipulation.cpp
@@ -9,24 +9,25 @@ long long int arr[N]; // globally declared arrays will be intialized to zero
 
 int main() 
 { 
-	long int m;
-	cin>>n>>m;
-	while(m--)
-	{
-		int a, ,b, d;
-		cin>>a>>b>>d;
-		arr[a] += d;
-		arr[b+1] -= d;
-		for(int i=1; i<=n; i++)
-		{
-			arr[i] +=arr[i-1];
-		}
-		long long int max = -1;
-		for(int i=1; i<=n; i++)
-		{
-			if(max < arr[i]) max = arr[i];
-		}
-		cout<<max<<endl;
-	}
-	return 0;
+    long int n, m;
+    cin>>n>>m;
+        while(m--)
+        {
+            int a, b, d;
+            cin>>a>>b>>d;
+            arr[a] += d;
+            arr[b+1] -= d;
+        }
+        for(int i=1; i<=n; i++)
+        {
+            arr[i] +=arr[i-1];
+        }
+        long long int max = -1;
+        for(int i=1; i<=n; i++)
+        {
+            if(max < arr[i]) max = arr[i];
+        }
+        cout<<max<<endl;
+    
+    return 0;
 }
