90f7c4795ecb3067efa6c5d9bd283eaae162e7a7
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-07-07 23:24:59 +0530

    DAY 25

---
+++ Codeforces/exciting_bets.cpp
@@ -0,0 +1,27 @@
+// problem link: https://codeforces.com/contest/1543/problem/A
+// idea: (during the contest) I tried the question for 4-5 test cases and luckily after a lot research got a pettern which all of them satisfied
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{ 
+	long int t;
+	cin>>t;
+	for(int i=0; i<t ; i++)
+	{
+		long long int a,b;
+        cin>>a>>b;
+        cout<<abs(a-b)<<" ";
+        long long int diff, count;
+        diff=abs(a-b);
+        
+        if(diff != 0)
+        {
+            long long int temp=(a % diff);
+            count=min(temp, abs(temp-diff));
+            cout<<count<<endl;
+        }
+        else cout<<"0"<<endl;
+	}
+	return 0;
+}
