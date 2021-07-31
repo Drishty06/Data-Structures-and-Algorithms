1e6529a20c405c37c06d84d2d1414ea179cea97a
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-07-31 22:38:19 +0530

    july lunchtime 2021

--- Codechef/chef_and_spells.cpp
+++ Codechef/chef_and_spells.cpp
@@ -1,1 +1,20 @@
 // problem link: https://www.codechef.com/LTIME98C/problems/CHFSPL
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{ 
+	long int t;
+	cin>>t;
+	for(int i=0; i<t; i++)
+	{
+		long int a, b, c, max;
+		cin>>a>>b>>c;
+		if(a+b > b+c) max = a+b;
+		else max = b+c;
+		if(max < a+c) max = a+c;
+		cout<<max<<endl;
+		
+	}
+	return 0;
+}
