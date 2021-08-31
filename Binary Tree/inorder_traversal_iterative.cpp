a7a101d457fb2e5ab8b4e3889df6df3094d80126
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-08-31 16:29:49 +0530

    DAY 53

--- Binary Tree/inorder_traversal_iterative.cpp
+++ Binary Tree/inorder_traversal_iterative.cpp
@@ -1,5 +1,7 @@
 // problem link: https://practice.geeksforgeeks.org/problems/inorder-traversal/1#
 // iterative solution
+// idea: while current node is not null, we keep traversing and keep storing elements into the stack. 
+//       Once we find find current node as null, we set top of stack as current node, print it, and set current node as curr->right
     vector<int> inOrder(Node* root) 
     {
        vector<int> ans;
