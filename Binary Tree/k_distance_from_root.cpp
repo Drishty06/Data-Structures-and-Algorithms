3a790c7be8646f249fa9364c0f9d4363891d5cff
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-09-04 13:54:39 +0530

    DAY 57

--- Binary Tree/k_distance_from_root.cpp
+++ Binary Tree/k_distance_from_root.cpp
@@ -1,33 +1,54 @@
 // problem link: https://practice.geeksforgeeks.org/problems/k-distance-from-root/1
+// method 1
 // simply used the code for level order traversal with a little change
 // added to vector only when idx = k
+// vector<int> Kdistance(struct Node *node, int k)
+// {
+//   vector<int> ans;
+//         queue<Node *> q;
+//         q.push(node);
+//         int idx=0;
+//         bool check = true;
+//         while(q.size() != 0)
+//         {
+//             int count = q.size();
+//             for(int i=0; i<count; i++)
+//             {
+//                 Node *top = q.front();
+//                 q.pop();
+//                 if(top != NULL) 
+//                 {
+//                     if(idx == k)
+//                     {
+//                         ans.push_back(top->data);
+//                         check = false;
+//                     }
+//                     if(top -> left) q.push(top->left);
+//                     if(top -> right) q.push(top->right);
+//                 }
+//             }
+//             if(check == false) break;
+//             idx++;
+//         }
+//         return ans;
+// }
+
+// method 2 recursive
+
+void solve(vector<int> &ans,struct Node *root, int k)
+{
+  if(root == NULL) return;
+  if(k == 0) ans.push_back(root->data);
+  else
+  {
+    solve(ans, root->left, k-1);
+    solve(ans, root->right, k-1);
+  }
+}
 vector<int> Kdistance(struct Node *node, int k)
 {
   vector<int> ans;
-        queue<Node *> q;
-        q.push(node);
-        int idx=0;
-        bool check = true;
-        while(q.size() != 0)
-        {
-            int count = q.size();
-            for(int i=0; i<count; i++)
-            {
-                Node *top = q.front();
-                q.pop();
-                if(top != NULL) 
-                {
-                    if(idx == k)
-                    {
-                        ans.push_back(top->data);
-                        check = false;
-                    }
-                    if(top -> left) q.push(top->left);
-                    if(top -> right) q.push(top->right);
-                }
-            }
-            if(check == false) break;
-            idx++;
-        }
-        return ans;
+  solve(ans, node, k);
+  return ans;
 }
+
