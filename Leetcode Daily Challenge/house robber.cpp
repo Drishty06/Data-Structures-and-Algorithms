4b21c9458f85c3c548729d69792191f50a874176
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-12-03 21:40:51 +0530

    December Challenge 2021

---
+++ Leetcode Daily Challenge/house robber.cpp
@@ -0,0 +1,20 @@
+int rob(vector<int>& nums) {
+        
+        int size = nums.size();
+        int oddSum = 0, evenSum = 0;
+        for(int i=0; i<size; i++)
+        {
+            if(i % 2 == 0)
+            {
+                evenSum +=nums[i];
+                evenSum = max(evenSum, oddSum);
+            }
+            else
+            {
+                oddSum += nums[i];
+                oddSum = max(evenSum, oddSum);
+            }
+        }
+        return max(oddSum, evenSum);
+        
+    }
