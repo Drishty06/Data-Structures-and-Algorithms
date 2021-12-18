26d660e4f38b5cf129e452b46c0c16ed0d4f0552
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2021-12-18 17:24:29 +0530

    DAY 89 - DAY 103

---
+++ Arrays/reverse pairs(inversion count).cpp
@@ -0,0 +1,37 @@
+// very similar question to inversion count
+    // https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
+    int count = 0;
+    void checkCount(vector<int>& nums, int start, int mid, int end){
+        // two pointers;
+        int l = start,r=mid + 1;
+        while(l <= mid && r <= end){
+            if((long)nums[l]>(long) 2 * nums[r]){
+                count+= (mid - l + 1);
+                r++;
+            }
+            else{
+                l++;
+            }
+        }
+       // worst case might be nlog(n) 
+       sort(nums.begin() + start, nums.begin() + end + 1);
+        return;
+        //every step sort
+    }
+    void mergeSort(vector<int>& nums, int start, int end){
+        if(start == end) return;
+        int mid = (start + end)/2;
+        mergeSort(nums,start, mid);
+        mergeSort(nums,mid+1,end);
+        
+        checkCount(nums,start,mid,end);
+        return;
+        
+    }
+
+    int reversePairs(vector<int>& nums) {
+        if(!nums.size())return 0;
+        count = 0;
+        mergeSort(nums,0,nums.size()-1);
+        return count;
+    }
