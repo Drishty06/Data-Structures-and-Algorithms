9f739137b273a42aa4ffe01f92bd936eb1e5d903
Drishty Ganatra <77919644+Drishty06@users.noreply.github.com>
2022-01-24 19:49:35 +0530

    DP 07 (notes pg 22-25)

---
+++ Dynamic Progamming/7 unbounded knapsack.cpp
@@ -0,0 +1,31 @@
+// notes pg: 23-25
+// just a minor change in lin 22
+int 01knapsack(vector<int>& wt, int W, int n)
+    {
+        int dp[n + 1][W + 1];
+        // initializing values
+        for(int i=0; i < n+1; i++)
+        {
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<W+1; i++)
+        {
+            dp[0][j] = 0;
+        }
+        // filing the matrix
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=1; j<W+1; j++)
+            {
+                if(wt[i-1] <= j)
+                {
+                    dp[i][j] = max(val[i-1] + dp[i][j - wt[i-1]], dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
